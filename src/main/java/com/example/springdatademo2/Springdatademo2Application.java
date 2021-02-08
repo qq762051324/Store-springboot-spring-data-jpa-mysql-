099905652ec6a115cfeac48521e8817088b3d74a
qq762051324 <wwcc00123@163.com>
2021-02-08 19:52:43 +0100

    init

---
+++ src/main/java/com/example/springdatademo2/Springdatademo2Application.java
@@ -0,0 +1,13 @@
+package com.example.springdatademo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springdatademo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springdatademo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdatademo2/Springdatademo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatademo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springdatademo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
